048d0d680395407626504ce35006c4c44e43c7d7
Guillaume Regimbeau <gregimbeau@mail.com>
2023-09-05 11:58:32 +0200

    multiple photos

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,16 +1,8 @@
-# config/initializers/cloudinary.rb
-# Cloudinary.config do |config|
-#   config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
-#   config.api_key = ENV['CLOUDINARY_API_KEY']
-#   config.api_secret = ENV['CLOUDINARY_API_SECRET']
-#   config.secure = true
-#   config.cdn_subdomain = true
-# end
-
+config/initializers/cloudinary.rb
 Cloudinary.config do |config|
-  config.cloud_name = "dmq3cpw6u"
-  config.api_key = "298317697953782"
-  config.api_secret = "buvE08NGeE9weBjCsQQUwUVph20"
+  config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
+  config.api_key = ENV['CLOUDINARY_API_KEY']
+  config.api_secret = ENV['CLOUDINARY_API_SECRET']
   config.secure = true
   config.cdn_subdomain = true
 end
